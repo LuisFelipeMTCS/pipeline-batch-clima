9a77228dcf5af609e35bee7ff136a24626f0afc9
LuisFelipeMTCS <luisfelipetoledo.profissional@gmail.com>
2026-02-12 20:16:33 -0300

    ingestao agricultura

--- dags/agricultura_dag.py
+++ dags/agricultura_dag.py
@@ -1,7 +1,29 @@
-"""DAG Pipeline Agricultura [TODO]"""
+"""DAG Pipeline agricultura (INMET)"""
+
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-with DAG("pipeline_agricultura", start_date=datetime(2024,1,1), schedule_interval="@monthly", catchup=False, tags=["agricultura"]) as dag:
-    DummyOperator(task_id="not_implemented")
+from src.ingestion.pipelines.agricultura import ingest_data
+
+
+with DAG(
+    dag_id="pipeline_agricultura",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["agricultura", "inmet"],
+    params={"year_filter": None, "max_workers": 10},
+) as dag:
+
+    def run_ingest(**context):
+        p = context["params"]
+        return ingest_data(
+            year_filter=p.get("year_filter"),
+            max_workers=p.get("max_workers", 10),
+        )
+
+    PythonOperator(
+        task_id="ingest_agricultura",
+        python_callable=run_ingest,
+    )
